e8b28295fff453971de2c585f8c1386d2a0f8e51
Vitor Scholze <vitorscholze7@gmail.com>
2026-09-20 10:28:15 -0300

    Adicionando anotacoes no metodo delete

--- src/main/java/com/example/financeiro/treino/controller/ContaController.java
+++ src/main/java/com/example/financeiro/treino/controller/ContaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,7 +47,8 @@ public class ContaController {
 
 
 
-    public ResponseEntity<String> deletarConta(Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarConta(@PathVariable Long id){
         contaService.deletarConta(id);
 
         return ResponseEntity.ok("Conta deletada com sucesso!");
